db456f8635b569d8cb98025e2ec30df7fb6e53a0
Niginabonu <117664347+Niginabonu@users.noreply.github.com>
2022-11-12 11:01:33 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            if (n % 400 == 0 || (n % 4 == 0 && n % 100 != 0))
+                Console.WriteLine(" 12/09/" + n);
+            else Console.WriteLine(" 13/09/" + n);
+            Console.ReadKey();
+        }
+    }
+}
